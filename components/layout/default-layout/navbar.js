09ee4cd84c1fea31c3c8307716ef951d841c0091
tseng24 <tyf29240@gmail.com>
2024-09-11 14:42:18 +0800

    navbar格式更新

--- components/layout/default-layout/navbar.js
+++ components/layout/default-layout/navbar.js
@@ -1,5 +1,38 @@
 import React from 'react'
 
+
 export default function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <>
+      <header>
+        <div className="logo">
+          <a href="#">會營ㄛ</a>
+        </div>
+        <div className="navbar">
+          <ul>
+            <li>
+              <a href="">尋找空位</a>
+            </li>
+            <li>
+              <a href="">露營用具</a>
+            </li>
+            <li>
+              <a href="">活動情報</a>
+            </li>
+            <li>
+              <a href="">客服中心</a>
+            </li>
+          </ul>
+        </div>
+        <div className="user">
+          <div className="userpic">
+            <a href="#">
+              <img src="/imgs/usagi.jpg" alt="" />
+            </a>
+          </div>
+          <p>兔兔</p>
+        </div>
+      </header>
+    </>
+  )
 }
