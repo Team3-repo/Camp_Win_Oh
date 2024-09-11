5ae4d78e6dc0692c0e89115f9831cbd1631d5253
tseng24 <tyf29240@gmail.com>
2024-09-11 22:05:32 +0800

    test

--- pages/a.js
+++ pages/a.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '@/components/layout/default-layout/navbar'
 
 export default function h1() {
   return (
     <>
+      <Navbar />
       <h1>aaaa</h1>
     </>
   )
