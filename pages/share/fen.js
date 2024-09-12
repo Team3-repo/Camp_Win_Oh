a4cf6fdf534e4aea1e2b76011e5d02af0164871f
tseng24 <tyf29240@gmail.com>
2024-09-12 15:10:37 +0800

    fen 更新

---
+++ pages/share/fen.js
@@ -0,0 +1,14 @@
+import Footer from '@/components/layout/default-layout/footer'
+import Navbar from '@/components/layout/default-layout/navbar'
+import Pagi from '@/components/layout/default-layout/pagi'
+import React from 'react'
+
+export default function Fen() {
+  return (
+    <>
+      <Navbar />
+      <Pagi/>
+      <Footer/>
+    </>
+  )
+}
