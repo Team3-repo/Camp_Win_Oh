05b163aeb0cd9535869cd193f5ab538f46b1e0d8
Ku-AA-stanley <plasma12563@gmail.com>
2024-09-14 14:59:17 +0800

    加上 nav footer

--- pages/user/profile.jsx
+++ pages/user/profile.jsx
@@ -1,18 +1,18 @@
 import FormField from '@/components/form/FormField'
 import React from 'react'
 import Button from '@/components/book/button'
+import Navbar from '@/components/event/navbar'
+import Footer1 from '@/components/event/footer1'
 
 export default function profile() {
   return (
     <>
+      <Navbar />
       <h1 className="nav">設定</h1>
 
       {/* 按鈕切換 */}
       <div className="button-group" style={{ display: 'flex', gap: '5px' }}>
-        <Button
-          label="個人資料"
-          onClick={() => alert('Button clicked!')}
-        />
+        <Button label="個人資料" onClick={() => alert('Button clicked!')} />
         <Button
           label="付款方式"
           onClick={() => alert('Button clicked!')}
@@ -26,9 +26,16 @@ export default function profile() {
       </div>
 
       {/* 圖片 */}
-      <section className="avatar-description" style={{ display: 'flex', gap: '5px' }}>
+      <section
+        className="avatar-description"
+        style={{ display: 'flex', gap: '5px' }}
+      >
         <div>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" alt="avatar" width={70}/>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+            alt="avatar"
+            width={70}
+          />
         </div>
         <div>
           <h4>個人資料圖片</h4>
@@ -64,16 +71,15 @@ export default function profile() {
       </section>
 
       <section style={{ display: 'flex', gap: '5px' }}>
-        <Button
-          label="取消"
-          onClick={() => alert('Button clicked!')}
-        />
+        <Button label="取消" onClick={() => alert('Button clicked!')} />
         <Button
           label="儲存變更"
           onClick={() => alert('Button clicked!')}
           type="btn-reg"
         />
       </section>
+
+      <Footer1 />
     </>
   )
 }
