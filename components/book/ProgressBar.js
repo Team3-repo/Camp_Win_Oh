60b2bec37b670e5d9ee8a48d6bf41835880ee6cc
wen0271 <yuwen000311@gmail.com>
2024-09-17 15:50:04 +0800

    購物車step-by-step流程

--- components/book/ProgressBar.js
+++ components/book/ProgressBar.js
@@ -1,24 +1,25 @@
 import React from 'react'
+import styles from '../../styles/ProgressBar.module.css'
 
 export default function ProgressBar() {
   return (
-    <div className="progress-container">
-      <div className="step">
-        <div className="circle">1</div>
+    <div className={styles.progressContainer}>
+      <div className={styles.step}>
+        <div className={styles.circle}>1</div>
         <p>選擇方案</p>
       </div>
 
-      <div className="line"></div>
+      <div className={styles.line}></div>
 
-      <div className="step">
-        <div className="circle">2</div>
+      <div className={styles.step}>
+        <div className={styles.circle}>2</div>
         <p>填寫資料</p>
       </div>
 
-      <div className="line"></div>
+      <div className={styles.line}></div>
 
-      <div className="step">
-        <div className="circle">3</div>
+      <div className={styles.step}>
+        <div className={styles.circle}>3</div>
         <p>完成付款</p>
       </div>
     </div>
