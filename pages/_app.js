f0fec058aa36476a9f84238ccea635ad167360a7
PengYang <plasma12563@gmail.com>
2024-09-18 11:26:31 +0800

    修正 import styles path

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '@/styles/globals.css'
-import '@/styles/Pagi.css'
-import '@/styles/Footer.css'
+import '@/styles/component_style/Pagi.css'
+import '@/styles/component_style/Footer.css'
 import '@/styles/EventStyle.css'
+
 
 export default function MyApp({ Component, pageProps }) {
   // 使用自訂在頁面層級的版面(layout)
