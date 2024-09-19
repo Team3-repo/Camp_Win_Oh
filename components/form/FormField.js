8d11defb800bde2fd6c710ead6cfc9a28ec73416
Ku-AA-stanley <plasma12563@gmail.com>
2024-09-19 23:54:19 +0800

    FormField 預設顏色

--- components/form/FormField.js
+++ components/form/FormField.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const FormField = ({ width = '50%', label, id, type, placeholder, required = false, value, onChange }) => (
+const FormField = ({
+  width = '50%',
+  label,
+  id,
+  type,
+  placeholder,
+  required = false,
+  value,
+  onChange,
+  backgroundColor = '#fff' // 新增 backgroundColor prop，預設值為白色
+}) => (
   <>
     <h2 style={{ fontSize: '1.3rem' }}>
       {label}
@@ -16,13 +26,14 @@ const FormField = ({ width = '50%', label, id, type, placeholder, required = fal
         style={{
           width: width, // 使用傳入的 width prop 或預設值
           borderRadius: '3.79px',
-          border: '1px solid #ccc', // 可選的邊框顏色
+          border: '1px solid #ccc', // 邊框顏色
           padding: '8px', // 可選的內邊距
-          boxSizing: 'border-box' // 使內邊距和邊框包括在元素的總寬度內
+          boxSizing: 'border-box', // 使內邊距和邊框包括在元素的總寬度內
+          backgroundColor: backgroundColor // 使用傳入的 backgroundColor prop 來設定底色
         }}
       />
     </label>
   </>
-)
+);
 
-export default FormField
+export default FormField;
