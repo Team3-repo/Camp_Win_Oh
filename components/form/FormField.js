4ad1931049fa164b45bdd66c022b297cddea4633
PengYang <plasma12563@gmail.com>
2024-09-19 15:20:12 +0800

    profile驗證

--- components/form/FormField.js
+++ components/form/FormField.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const FormField = ({ width = '50%', label, id, type, placeholder, required = false }) => (
+const FormField = ({ width = '50%', label, id, type, placeholder, required = false, value, onChange }) => (
   <>
     <h2 style={{ fontSize: '1.3rem' }}>
       {label}
@@ -11,6 +11,8 @@ const FormField = ({ width = '50%', label, id, type, placeholder, required = fal
         id={id}
         type={type}
         placeholder={placeholder}
+        value={value}
+        onChange={onChange}
         style={{
           width: width, // 使用傳入的 width prop 或預設值
           borderRadius: '3.79px',
