343b6ff6a45999c3456e5c95946fd37c7250fa2c
tseng24 <tyf29240@gmail.com>
2024-09-23 15:33:39 +0800

    活動頁面更新

--- components/event/footer2.js
+++ components/event/footer2.js
@@ -6,6 +6,21 @@ export default function Footer2() {
       <footer className="footer2">
         <h6>Copyright © 2024 會營お | CampWinOh Campground</h6>
       </footer>
+      <style jsx>
+        {`
+          .footer2 {
+            display: flex;
+            height: 50px;
+            background-color: #98d293;
+            justify-content: center;
+            align-items: center;
+            font-weight: normal;
+          }
+          .footer2 h6 {
+            font-family: 'Zen Maru Gothic', sans-serif;
+          }
+        `}
+      </style>
     </>
   )
 }
